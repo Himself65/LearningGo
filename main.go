326a8a22e55cabbc3f5bc7b9ed0747344ca9ff0c
himself65 <himself65@outlook.com>
2019-05-28 23:13:51 +0800

    update: use var and fmt.Println

--- main.go
+++ main.go
@@ -5,3 +5,22 @@ import "fmt"
 func printSomething() {
 	fmt.Println("Hello, world")
 }
+
+func varSometing() {
+	a := 1
+	fmt.Println(a)
+
+	b := "Stirng"
+	fmt.Println(a, b)
+
+	// mark: '\n' is different from "\n"
+	fmt.Println()
+	fmt.Print(a, b, "\n")
+
+	fmt.Println()
+	fmt.Print(a, b, '\n')
+}
+
+func main() {
+	varSometing()
+}
